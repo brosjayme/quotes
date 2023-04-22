a8e04e23d2739f0942d267be5b11ac0e920a0e0b
unknown <brosjayme@gmail.com>
2023-04-22 11:14:58 +0100

    added quote api to the js file

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ quotBtn = document.querySelector('button');
 
 
 function randomQuote() {
-    console.log('Clicked')
+    // this fetches random quotes/data from the API and parsing it into javaScript object
+fetch('https://api.quotable.io/random').then(res => res.json()).then( result => {
+console.log(result)
+quoteText.innerText = result.content;
+})
 }
 
 quotBtn.addEventListener('click', randomQuote);
