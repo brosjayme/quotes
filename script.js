1c0956c33455da18240c33a02ed299ac051a0bcc
unknown <brosjayme@gmail.com>
2023-04-22 10:57:54 +0100

    added js file to the html page

---
+++ script.js
@@ -0,0 +1,9 @@
+const quoteText =document.querySelector('.quote'),
+quotBtn = document.querySelector('button');
+
+
+function randomQuote() {
+    console.log('Clicked')
+}
+
+quotBtn.addEventListener('click', randomQuote);
